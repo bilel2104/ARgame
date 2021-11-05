832ba46d34d753331ae164ea808837f6f6c708f9
bilel dhouaibi <bilel.dhouaibi@esprit.tn>
2021-11-05 09:50:39 +0100

    initialisation

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,105 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1.0f;
+
+    [SerializeField]
+    private float torque = 1.0f;
+
+    [SerializeField]
+    private float minSpeedBeforeTorque = 0.3f;
+
+    [SerializeField]
+    private float minSpeedBeforeIdle = 0.2f;
+
+    [SerializeField]
+    private Rigidbody carRigidBody = null;
+
+    #region Stats
+
+    [SerializeField]
+    private bool showStats = false;
+
+    [SerializeField]
+    private TextMeshProUGUI speedText = null;
+
+    #endregion
+
+    private CarWheel[] wheels;
+
+    public enum Direction
+    {
+        Idle,
+        MoveForward,
+        MoveBackward,
+        TurnLeft,
+        TurnRight
+    }
+
+    void Awake()
+    {
+        wheels = GetComponentsInChildren<CarWheel>();
+#if UNITY_EDITOR
+        PlayerInputController.Instance.Bind(this);
+#endif
+    }
+
+    void Update()
+    {
+        if (carRigidBody.velocity.magnitude <= minSpeedBeforeIdle)
+        {
+            AddWheelsSpeed(0);
+        }
+
+        if(showStats)
+        {
+            if(speedText != null)
+            {
+                //magnitude is in 1 meter per second
+                //convert it to miles per hour by * 2.236936
+                speedText.text = $"{string.Format("{0:0.#}", carRigidBody.velocity.magnitude * 2.236936)} Mph";
+            }
+        }
+    }
+
+    public void Accelerate()
+    {
+        carRigidBody.AddForce(transform.forward * speed, ForceMode.Acceleration);
+        AddWheelsSpeed(speed);
+    }
+
+    public void Reverse()
+    {
+        carRigidBody.AddForce(-transform.forward * speed, ForceMode.Acceleration);;
+        AddWheelsSpeed(-speed);
+    }
+
+    public void TurnLeft()
+    {
+        if(canApplyTorque())
+            carRigidBody.AddTorque(transform.up * -torque);
+    }
+
+    public void TurnRight()
+    {
+        if(canApplyTorque())
+            carRigidBody.AddTorque(transform.up * torque);
+    }
+
+    void AddWheelsSpeed(float speed)
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.WheelSpeed = speed;
+        }
+    }
+
+    bool canApplyTorque()
+    {
+        Vector3 velocity = carRigidBody.velocity;
+        return Mathf.Abs(velocity.x) >= minSpeedBeforeTorque || Mathf.Abs(velocity.z) >= minSpeedBeforeTorque;
+    }
+}
---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,64 @@
+﻿using DilmerGames.Core.Singletons;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerInputController : Singleton<PlayerInputController>
+{
+    private bool turnLeft, turnRight, accelerate, reverse;
+    
+    private CarController carController;
+
+    public void Bind(CarController carController)
+    {
+        this.carController = carController;
+    }
+
+    void FixedUpdate() 
+    {
+        if(carController == null) 
+        {
+            return;
+        }
+
+        if(accelerate)
+        {
+            carController.Accelerate();
+        }
+        if(reverse)
+        {
+            carController.Reverse();
+        }
+        if(turnLeft)
+        {
+            carController.TurnLeft();
+        }
+        if(turnRight)
+        {
+            carController.TurnRight();
+        }
+    }
+
+    public void OnTurnLeft(InputValue inputValue) 
+    { 
+        turnLeft = inputValue.isPressed;
+        Logger.Instance.LogInfo($"OnTurnLeft...{turnLeft}");
+    }
+
+    public void OnTurnRight(InputValue inputValue) 
+    { 
+        turnRight = inputValue.isPressed;
+        Logger.Instance.LogInfo($"OnTurnRight...{turnRight}");
+    }
+
+    public void OnAccelerate(InputValue inputValue) 
+    {
+        accelerate = inputValue.isPressed;
+        Logger.Instance.LogInfo($"OnAccelerate...{accelerate}");
+    } 
+
+    public void OnReverse(InputValue inputValue) 
+    {
+        reverse = inputValue.isPressed;
+        Logger.Instance.LogInfo($"OnReverse...{reverse}");
+    }
+}
